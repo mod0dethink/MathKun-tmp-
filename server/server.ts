c629f3c8dc686ec6218538fb8902a03b81c362eb
mod0dethink <tanakaryuto1994@gmail.com>
2025-11-02 17:48:33 +0000

    added proper settings on server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import test from 'node:test';
 
 const app = express();
 const port = 8000;
@@ -9,10 +10,15 @@ app.use(express.json());
 
 // ルートエンドポイント
 app.get('/', (req: Request, res: Response) => {
-  res.send('hi UNTI can you see the change?');
+  res.send('hi UNTaaああああaaaaaI can you aaasee the change?');
 });
 
-// サーバーの起動
-app.listen(port, () => {
+app.get('/api/data', (req: Request, res: Response) => {
+  res.json({ message: 'Hello from the server!',
+            test: 'This is test message.' });
+});
+
+// サーバーの起動(dockerコンテナ内で動かすからlocalhostじゃなくて0.0.0.0にバインドしてる")
+app.listen(port,"0.0.0.0" ,() => {
   console.log(`Server is running at http://localhost:${port}`);
 });
