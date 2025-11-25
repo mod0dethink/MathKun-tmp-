c673f7cbf3e40ef003d4ccff2d71f3388792698d
mod0dethink <tanakaryuto1994@gmail.com>
2025-11-25 07:08:57 +0000

    cormiddleware追加

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"example.com/mathkun-tmp-/server/models"
 	"example.com/mathkun-tmp-/server/router"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -20,6 +21,13 @@ func main() {
 	db.Init()
 	db.DB.AutoMigrate(&models.User{})
 	r := gin.Default()
+	config := cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}
+	r.Use(cors.New(config))
 	router.SetupRouter(r)
 	r.Run(":8000")
 }
